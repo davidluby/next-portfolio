5df400c41425910466aaf8faea57d3cdc6602e68
davidluby <davidluby273@gmail.com>
2023-04-12 19:13:18 -0400

    simplified logic for add button

--- src/components/decks/Add.js
+++ src/components/decks/Add.js
@@ -1,23 +1,19 @@
 import React, { useState } from 'react'
 
 export default function Add({ playerData, cards, setCards, setEmpty }) {
-    const [full, setFull] = useState(false);
     const [text, setText] = useState("Add Card to Deck")
     const add = () => {
-        if (cards.length < 5) {
+        if (cards.length < 4) {
             setText("Add Card to Deck")
             setCards([...cards, playerData]);
             setEmpty(false);
-            setFull(false);
         } else{
-            setFull(true);
             setText("Max Five Cards")
         }
     };
   return (
     <button className="w-max py-2 px-3 rounded-full bg-green-700 hover:bg-green-600 text-white font-bold transition all duration-500"
-            onClick={add}
-            disabled={full}>
+            onClick={add}>
         {text}
     </button>
   )
