3ede2fde30f317b46d3903ec7da807193c5b0a2a
davidluby <davidluby273@gmail.com>
2023-04-12 19:26:38 -0400

    chagned add button feedback to be inside useeffect

--- src/components/decks/Add.js
+++ src/components/decks/Add.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Add({ playerData, cards, setCards, setEmpty }) {
-    const [text, setText] = useState("Add Card to Deck")
+    const [full, setFull] = useState(false);
+    const [text, setText] = useState("Add Card to Deck");
+
     const add = () => {
-        if (cards.length < 4) {
-            setText("Add Card to Deck")
+        if (cards.length < 5) {
             setCards([...cards, playerData]);
             setEmpty(false);
-        } else{
-            setText("Max Five Cards")
-        }
+        };
     };
+
+    useEffect(() => {
+        if (cards.length < 5) {
+            setText("Add Card to Deck");
+            setFull(false);
+        } else{
+            setText("Max Five Cards");
+            setFull(true);
+        };
+    }, [cards])
+
   return (
     <button className="w-max py-2 px-3 rounded-full bg-green-700 hover:bg-green-600 text-white font-bold transition all duration-500"
-            onClick={add}>
+            onClick={add}
+            disabled={full}>
         {text}
     </button>
   )
