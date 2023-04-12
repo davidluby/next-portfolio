e5fb6f850760247cbb42dbd206aee2ec1bbcafc5
davidluby <davidluby273@gmail.com>
2023-04-12 18:56:32 -0400

    fixed logic for good this time?

--- src/components/decks/ExpandCards.js
+++ src/components/decks/ExpandCards.js
@@ -1,20 +1,19 @@
 import React, { useState } from 'react'
 
 export default function ExpandCards() {
-	const [text, setText] = useState("Slide Deck")
+	const [text, setText] = useState("Stack Deck")
     const expand = () => {
 		const stack = document.querySelector("#expand");
 		stack.classList.toggle("-space-x-72");
 		stack.classList.toggle("-space-x-[21.5rem]");
-		if (text == "Fan Deck"){
+
+		if (text == "Stack Deck"){
 			setText("Fan Deck")
 		}
 		else {
-			setText("Slide Deck")
+			setText("Stack Deck")
 		}
-
     };
-    
 
   return (
 	<button className="bg-green-700 hover:bg-green-600 transition all duration-500 text-white font-bold py-2 px-4 rounded-full"
