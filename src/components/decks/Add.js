38a175a2a27057a0ffe7d0264bcb1380fd681586
davidluby <davidluby273@gmail.com>
2023-04-12 19:04:07 -0400

    disabling add to deck button when cards > 5

--- src/components/decks/Add.js
+++ src/components/decks/Add.js
@@ -1,17 +1,20 @@
 import React from 'react'
 
 export default function Add({ playerData, cards, setCards, setEmpty }) {
+    const [full, setFull] = useState(false);
     const add = () => {
         if (cards.length < 5) {
             setCards([...cards, playerData]);
             setEmpty(false);
+            setFull(false);
         } else{
-            console.log("Too many cards");
+            setFull(true);
         }
     };
   return (
     <button className="w-max py-2 px-3 rounded-full bg-green-700 hover:bg-green-600 text-white font-bold transition all duration-500"
-            onClick={add}>
+            onClick={add}
+            disabled={full}>
         Add card to Deck
     </button>
   )
