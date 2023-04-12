501a11a7e1bfef8efe0b68430bee59e51ff5fd13
davidluby <davidluby273@gmail.com>
2023-04-12 18:32:20 -0400

    search timeout done

--- src/components/decks/Search.js
+++ src/components/decks/Search.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 export default function Search({ setPlayerData, setSearching, setHidden }) {
     const [name, setName] = useState("");
     const [active, setActive] = useState(false);
+    const [placeholder, setPlaceholder] = useState("Search")
 
 
     const handleSubmit = event => {
@@ -26,23 +27,24 @@ export default function Search({ setPlayerData, setSearching, setHidden }) {
                     setSearching(false);
                     setHidden(false);
                     setActive(true);
-                    alert("The search function will be disabled for the next minute. Consider adding this card five times to save time and get the full picture.")
+                    setPlaceholder("Search function disabled for one minute. Condsider adding this card five times and editing later.")
                     setTimeout(() => {
                         setActive(active => !active)
-                    }, 5000);
+                        setPlaceholder("Search")
+                    }, 60000);
                 }
             )
             )
         }
     }
-
+    
     return (
         <div className="flex flex-col items-center w-1/2 relative">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             </div>
             <input className="w-full py-2 px-10 border-2 border-gray-300 shadow-md rounded-full transition all duration-500 hover:border-yellow-500 focus:outline-none focus:border-yellow-500"
-                placeholder="Search"
+                placeholder={placeholder}
                 onChange={handleSubmit}
                 value={name}
                 onKeyPress={searchName}
