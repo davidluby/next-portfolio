5f802fa1b0ee26e631118344fe6f179f7eb76e85
davidluby <davidluby273@gmail.com>
2023-05-02 03:55:47 -0400

    cleanup

--- src/pages/mech.js
+++ src/pages/mech.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import Head from 'next/head'
 
-function mech() {
-  return (
-    <div>coming soon</div>
-  )
-}
+export default function fluids() {
 
-export default mech
+
+    return (
+        <div className="flex flex-col items-center mb-20">
+            <Head>
+                <title>Mechatronics</title>
+            </Head>
+            <div className="flex flex-col items-center w-[97%] space-y-5 res:w-1/2 tile bg-slate-900">
+                <img src="mech/diagram.png"></img>
+                <img src="mech/block.jpg"></img>
+                <img src="mech/MCU.png"></img>
+                <img src="mech/platform.png"></img>
+            </div>
+        </div>
+      )
+  }
